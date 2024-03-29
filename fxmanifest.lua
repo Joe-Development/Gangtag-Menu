5fa82881ff2fef705c5a152f8c43e2d7a083dccc
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2024-03-29 03:03:06 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,14 +2,15 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'JaredScar & JoeV2'
-description 'Badger\'s DiscordTagIDs with A Headtag Menu'
-version '1.2.2'
+description 'Badger\'s DiscordTagIDs with A Gangtag Menu & A HUD'
+version '1.2.3'
 lua54 'yes'
 
 client_scripts {
     '@NativeUI/NativeUI.lua', 
     'config.lua',
     'client.lua',
+    'hud.lua'
 }
 server_scripts {
     "config.lua",
@@ -21,7 +22,3 @@ server_exports {
     "ShowUserTag"
 }
 
-escrow_ignore {
-    'stream/*.ytd',
-    'config.lua'
-}
